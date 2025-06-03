c0bbd32b6091b3dd4db56650141720772e8b069e
Jimmy1992x <jimmy_fernandez_92@hotmail.com>
2025-06-03 18:19:11 -0500

    Actualizamos el README

--- README.md
+++ README.md
@@ -1,18 +1,16 @@
-## Getting Started
+# Juego - Ahorcado - JAVA
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+Se realizó un juego en JAVA, donde el usuario debe ir colocando letra por letra en consola y tiene cierta cantidad de intentos. Si completa la palabra es ganador, de lo contrario termina los intentos y pierde.
 
-## Folder Structure
 
-The workspace contains two folders by default, where:
 
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
+# ¿Qué herramientas utilizamos?
+1.  Clase Scanner
+2. Declaraciones y asignaciones
+3. Arreglos
+4. Estructura de control: Iterativa (Bucle)
+5. Clase String y sus métodos valueOf, charAt y lenght()
+6. Clase Character y su métodoto toLowerCase
+7. Esctructura de control: condicional
 
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
-
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+[https://www.linkedin.com/in/jimmy-jhon-fernandez-zavaleta-3a92b0ab/]
--- src/Ahorcado.java
+++ src/Ahorcado.java
@@ -15,54 +15,44 @@ public class Ahorcado{
       //Arreglo para almacenar las letras adivinadas
       char[] letrasAdivinadas = new char[palabraSecreta.length()];
 
-      //Estructura de control iterativa para inicializar el arreglo de letras adivinadas
-      // y mostrar el estado inicial del juego.
-      //Bucle for para recorrer cada letra de la palabra secreta
+      // Inicializar el arreglo de letras adivinadas con guiones bajos
       for (int i = 0; i < letrasAdivinadas.length; i++) {
-         letrasAdivinadas[i] = '_'; // Inicializar con guiones bajos
-         System.out.println(letrasAdivinadas[i]);
+         letrasAdivinadas[i] = '_';
       }
     
-     //Estructura de control iterativa (bucle while) para permitir al usuario adivinar letras
-     while (!palabraAdivinada && intentos < intentosMaximos) {
-         // Esto se usa para mostrar el estado actual de las letras adivinadas
-         System.out.println("Palabra a adivinar "  + String.valueOf(letrasAdivinadas) + " (" +palabraSecreta.length() + " letras)");
+      // Bucle while para permitir al usuario adivinar letras
+      while (!palabraAdivinada && intentos < intentosMaximos) {
+         // Mostrar el estado actual de las letras adivinadas
+         System.out.println("Palabra a adivinar: "  + String.valueOf(letrasAdivinadas) + " (" + palabraSecreta.length() + " letras)");
 
          System.out.println("Introduce una letra, por favor:");
          // Leer la letra ingresada por el usuario
          char letra = Character.toLowerCase(scanner.next().charAt(0)); // Convertir a minúscula para evitar problemas de mayúsculas
          boolean letraCorrecta = false;
 
-         //Estructura de control iterativa para verificar si la letra adivinada está en la palabra secreta
+         // Verificar si la letra adivinada está en la palabra secreta
          for (int i = 0; i < palabraSecreta.length(); i++) {
-            //Estructura de control condicional para comparar la letra ingresada con cada letra de la palabra secreta
             if (palabraSecreta.charAt(i) == letra) {
                letrasAdivinadas[i] = letra; // Actualizar la letra adivinada en el arreglo
-               letraCorrecta= true; // Marcar que la letra fue encontrada
+               letraCorrecta = true; // Marcar que la letra fue encontrada
             }
          }
 
-         //Verificar si la letra fue encontrada y actualizar el estado del juego
+         // Verificar si la letra fue encontrada y actualizar el estado del juegogi
          if (!letraCorrecta) {
             intentos++; // Incrementar el contador de intentos si la letra no está en la palabra
             System.out.println("¡Incorrecto! Te quedan " + (intentosMaximos - intentos) + " intentos.");
-
          } 
          if (String.valueOf(letrasAdivinadas).equals(palabraSecreta)) {
             palabraAdivinada = true; // Si todas las letras han sido adivinadas, marcar la palabra como adivinada
             System.out.println("¡Felicidades! Has adivinado la palabra secreta: " + palabraSecreta);
          } 
-            
-      
-     }
+      }
 
-        if (!palabraAdivinada) {
+      if (!palabraAdivinada) {
          System.out.println("¡Qué pena te has quedado sin intentos! GAME OVER ");
-         //Cierre del scanner para liberar recursos
-         scanner.close();
-      
-     }
+      }
+      //Cierre del scanner para liberar recursos
+      scanner.close();
    }
-
-   }
-
+}
